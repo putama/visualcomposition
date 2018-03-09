fix numerical error in eval, train on objects without adjective

composition_evaluation.py
@@ -11,14 +11,16 @@ from vocabulary import Vocabulary
 from dataset_coco import get_transform
 from torch.utils import data
 from torch.autograd import Variable
+import torchvision.transforms as transforms
 
 from operator import itemgetter
-
+from PIL import Image, ImageDraw, ImageFont
+from tensorboardX import SummaryWriter
 
 def main():
     print('evaluate vse on visual composition...')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', default='runs/cocomit_vse++/model_best.pth.tar')
+    parser.add_argument('--model_path', default='runs/composition_session4_allobjects_batch64_decay5/epoch_6_checkpoint.pth.tar')
     parser.add_argument('--data_root', default='data/mitstates_data')
     parser.add_argument('--image_data', default='mit_image_data.pklz')
     parser.add_argument('--labels_train', default='split_labels_train.pklz')
@@ -26,7 +28,7 @@ def main():
     parser.add_argument('--meta_data', default='split_meta_info.pklz')
     parser.add_argument('--vocab_path', default='data/vocab')
     parser.add_argument('--crop_size', default=224)
-    parser.add_argument('--batch_size', default=48)
+    parser.add_argument('--batch_size', default=128)
     args = parser.parse_args()
     print(args)
 
@@ -67,6 +69,9 @@ def main():
     dataloader = data.DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True,
                                  collate_fn=data_utils.custom_collate)
 
+    writer = SummaryWriter(log_dir='/'.join(args.model_path.split('/')[0:2]))
+    pil2tensor = transforms.Compose([transforms.ToTensor()])
+
     allobjattsvecs = []
     for i, objatts in enumerate(dataset.get_all_pairs()):
         print '{} phrases encoded to vectors'.format(i * 64)
@@ -100,33 +105,48 @@ def main():
         imgvecs = model.img_enc(Variable(images))
         imgvecs = imgvecs.data.cpu().numpy()
 
-        targetdist = np.einsum('ij,ij->i', imgvecs, objattsvecs)
+        targetdist = np.around(np.einsum('ij,ij->i', imgvecs, objattsvecs), decimals=5)
+
         allobjattdist = np.dot(imgvecs, allobjattsvecs.T)
-        sorteddist = np.sort(allobjattdist)
-        top10pred = sum(targetdist > sorteddist[:, -10])
-        top5pred = sum(targetdist > sorteddist[:, -5])
-        top3pred = sum(targetdist > sorteddist[:, -3])
-        top2pred = sum(targetdist > sorteddist[:, -2])
-        top1pred = sum(targetdist > sorteddist[:, -1])
+        sorteddist = np.around(np.sort(allobjattdist), decimals=5) # handle numerical instability
+
+        indices = np.argsort(allobjattdist[-1])[::-1]
+        top3 = indices[0:3]
+
+        topcaptions = map(lambda x: imgmetadata['pairNames'][x].lower(), top3)
+        print imgpaths[-1].split('/')[-2] + ' => ' + str(topcaptions)
+
+        pilimage = Image.open(imgpaths[-1]).resize((248,248))
+        captimage = Image.new('RGB', pilimage.size, color = (255,255,255))
+        drawcapt = ImageDraw.Draw(captimage)
+        drawcapt.text((10,10), "Paired phrase:", fill=(0, 0, 0))
+        drawcapt.text((10,30), '+ '+imgpaths[-1].split('/')[-2], fill=(0,0,0))
+        drawcapt.text((10,50), "Retrieved phrase:", fill=(0, 0, 0))
+
+        for counter, topcapt in enumerate(topcaptions):
+            drawcapt.text((10, 10+(20*(counter+3))), '- '+topcapt, fill=(0,0,0))
+
+        newimg = Image.new('RGB', (pilimage.size[0] * 2, pilimage.size[1]))
+        newimg.paste(pilimage, (0, 0))
+        newimg.paste(captimage, (pilimage.size[0], 0))
+        writer.add_image('Image-'+str(i), pil2tensor(newimg), 0)
+
+        top10pred = sum(targetdist >= sorteddist[:, -10])
+        top5pred = sum(targetdist >= sorteddist[:, -5])
+        top3pred = sum(targetdist >= sorteddist[:, -3])
+        top2pred = sum(targetdist >= sorteddist[:, -2])
+        top1pred = sum(targetdist >= sorteddist[:, -1])
         totaltop10 += top10pred
         totaltop5 += top5pred
         totaltop3 += top3pred
         totaltop2 += top2pred
         totaltop1 += top1pred
-        if top1pred > 0:
-            foundpaths = itemgetter(*np.where(targetdist > sorteddist[:, -1])[0])(imgpaths)
-            if isinstance(foundpaths, basestring):
-                correctpaths.append(foundpaths)
-            else:
-                correctpaths.extend(foundpaths)
 
     print 'top-1 accuracy: {}, top-2 accuracy: {}, top-3 accuracy: {}'.format(
         totaltop1 * 100 / len(dataset), totaltop2 * 100 / len(dataset), totaltop3 * 100 / len(dataset))
     print 'top-5 accuracy: {}, top-10 accuracy: {}'.format(
         totaltop5 * 100 / len(dataset), totaltop10 * 100 / len(dataset))
-    import pdb;
-    pdb.set_trace()
-
+    writer.close()
 
 if __name__ == '__main__':
     main()

composition_training.py
@@ -32,15 +32,18 @@ def main():
     parser.add_argument('--labels_test', default='split_labels_test.pklz')
     parser.add_argument('--meta_data', default='split_meta_info.pklz')
     parser.add_argument('--vocab_path', default='data/vocab')
-    parser.add_argument('--crop_size', default=224)
-    parser.add_argument('--batch_size', default=48)
+    parser.add_argument('--logger_name', default='runs/composition_session4_allobjects_batch64_decay5',
+                        help='Path to save the model and Tensorboard log.')
+    parser.add_argument('--lr_update', default=5, type=int)
+    parser.add_argument('--num_epochs', default=30, type=int)
+    parser.add_argument('--learning_rate', default=.0002, type=float)
+    parser.add_argument('--batch_size', default=64, type=int)
 
     args = parser.parse_args()
     print(args)
 
     imgdata = im_utils.load(args.data_root + '/' + args.image_data)
     labelstrain = im_utils.load(args.data_root + '/' + args.labels_train)
-    labelstest = im_utils.load(args.data_root + '/' + args.labels_test)
     imgmetadata = im_utils.load(args.data_root + '/' + args.meta_data)
 
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
@@ -74,14 +77,16 @@ def main():
     print('=> VSE model initiated and weights loaded')
 
     # load mitstates dataset
-    dataset = data_utils.MITstatesDataset(args.data_root, labelstest,
+    dataset = data_utils.MITstatesDataset(args.data_root, labelstrain,
                                           imgdata, imgmetadata, vocab,
                                           transform=get_transform('train', opt))
     dataloader = data.DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True,
                                  collate_fn=data_utils.custom_collate)
 
     print("Forward CNN using ", torch.cuda.device_count(), " GPUs!")
-    for epoch in range(opt.num_epochs):
+    for epoch in range(args.num_epochs):
+        adjust_learning_rate(args, model.optimizer, epoch)
+
         # average meters to record the training statistics
         batch_time = AverageMeter()
         data_time = AverageMeter()
@@ -97,7 +102,6 @@ def main():
 
             # make sure train logger is used
             model.logger = train_logger
-
             model.train_emb(images, objatts, lengths)
 
             # Print log info
@@ -118,14 +122,19 @@ def main():
             tb_logger.log_value('data_time', data_time.val, step=model.Eiters)
             model.logger.tb_log(tb_logger, step=model.Eiters)
 
-        savepath = os.path.join(opt.logger_name,
-                                'epoch_{}_checkpoint.pth.tar'.format(epoch+1))
-        torch.save({
-            'epoch': epoch + 1,
-            'model': model.state_dict(),
-            'opt': opt,
-            'Eiters': model.Eiters,
-        }, savepath)
+        if epoch % 5 == 0:
+            savepath = os.path.join(args.logger_name, 'epoch_{}_checkpoint.pth.tar'.format(epoch+1))
+            torch.save({
+                'epoch': epoch + 1,
+                'model': model.state_dict(),
+                'opt': opt,
+                'Eiters': model.Eiters,
+            }, savepath)
+
+def adjust_learning_rate(opt, optimizer, epoch):
+    lr = opt.learning_rate * (0.1 ** (epoch // opt.lr_update))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
 if __name__ == '__main__':
     main()

dataset_mit_states.py
@@ -16,7 +16,10 @@ class MITstatesDataset(data.Dataset):
 
     def __getitem__(self, index):
         # load image
-        imgid = self.splitdata['imIds'][index]
+        if self.splitdata.has_key('allTrainObImIds'):
+            imgid = self.splitdata['allTrainObImIds'][index]
+        else:
+            imgid = self.splitdata['imIds'][index]
         imgpath = self.imgdata['images'][imgid]['file_name']
         imgpathfull = os.path.join(self.root, 'images', imgpath.replace('_',' ',1))
         image = Image.open(imgpathfull).convert('RGB')
@@ -24,13 +27,17 @@ class MITstatesDataset(data.Dataset):
             image = self.transform(image)
 
         # load att-obj phrase
-        objatt_id = self.imgdata['annotations'][imgid]['pair_labs'][0]
-        objatt_name = self.imgmetadata['pairNames'][objatt_id]
-        objatt_tensor = self.text_to_ids(objatt_name)
+        if len(self.imgdata['annotations'][imgid]['pair_labs']) == 0:
+            obj_id = self.imgdata['annotations'][imgid]['ob_labs'][0]
+            obj_name = self.imgmetadata['objects'][obj_id]
+            objatt_tensor = self.text_to_ids(obj_name)
+        else:
+            objatt_id = self.imgdata['annotations'][imgid]['pair_labs'][0]
+            objatt_name = self.imgmetadata['pairNames'][objatt_id]
+            objatt_tensor = self.text_to_ids(objatt_name)
 
         if objatt_tensor is None:
             return None, None, None, None
-
         return image, objatt_tensor, imgid, imgpathfull
 
     def text_to_ids(self, phrase):
@@ -65,14 +72,24 @@ class MITstatesDataset(data.Dataset):
 
 def custom_collate(items):
     try: # case where at least 1 item is not UNKED
+        items.sort(key=lambda x: len(x[1]), reverse=True)
         items = filter(lambda x: x[0] is not None, items)
-        images, objatt_tensors, imgids, imgpaths = zip(*items)
-        lengths = [len(phrase) for phrase in objatt_tensors]
+        
+        images, objatt_tuples, imgids, imgpaths = zip(*items)
+        lengths = [len(phrase) for phrase in objatt_tuples]
 
         # stack images and objatt phrase into a batch
         images = torch.stack(images, 0)
-        objatt_tensors = torch.stack(objatt_tensors, 0).long()
+
+        # tuples of 1D tensors to 2D tensors
+        objatt_tensors = torch.zeros(len(objatt_tuples), max(lengths)).long()
+        for i, objatt in enumerate(objatt_tuples):
+            end = lengths[i]
+            objatt_tensors[i, :end] = objatt[:end]
+
+        # objatt_tensors = torch.stack(objatt_tensors, 0).long()
 
         return images, objatt_tensors, lengths, imgids, imgpaths
     except:
+        print 'EXCEPTION!'
         return None, None, None, None, None